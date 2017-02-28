8e650bcac5f0097a8162978052b3ca54e10ad582
Julius Williams <doctorj89@gmail.com>
2017-02-28 10:05:32 -0500

    cleaned up code

--- dashboard.js
+++ dashboard.js
@@ -4,11 +4,17 @@ console.log(`Number of records: ${numberOfRecords}`);
 
 
 // 2. Set each dashboard image to different images, one for each season. Make sure you also set the alt="" attribute with an appropriate description. 
-var images = document.querySelectorAll('img');  
-images[0].src = "https://robohash.org/winter";
-images[1].src = "https://robohash.org/spring";
-images[2].src = "https://robohash.org/summer";
-images[3].src = "https://robohash.org/fall";
+var images = document.querySelectorAll('img');
+
+changeSource(0, 'winter', 'Winter');
+changeSource(1, 'spring', 'Spring');
+changeSource(2, 'summer', 'Summer');
+changeSource(3, 'fall', 'Fall');
+
+function changeSource(index, season, alt) {
+    images[index].src = `https://robohash.org/${season}`;
+    images[index].alt = alt;
+}
 
 // 3. Below each dashboard image, there's a season/quarter name. Below those, there are three dots. Replace those three dots with a randomly generated number formatted as currency, like $12,589.00.
 var placeholders = document.querySelectorAll('.placeholder');
@@ -22,15 +28,15 @@ var randomNumber2 = "$" + numberWithCommas(Math.round(Math.random() * 1500000) /
 var randomNumber3 = "$" + numberWithCommas(Math.round(Math.random() * 1500000) / 100);
 var randomNumber4 = "$" + numberWithCommas(Math.round(Math.random() * 1500000) / 100);
 
-function replaceDots(quarter, text) {
-var dots = placeholders[quarter - 1].children[2];
-dots.innerText = text;
-}
-
 replaceDots(1, randomNumber1);
 replaceDots(2, randomNumber2);
 replaceDots(3, randomNumber3);
 replaceDots(4, randomNumber4);
+
+function replaceDots(quarter, text) {
+    var dots = placeholders[quarter - 1].children[2];
+    dots.innerText = text;
+}
 
 
 // 4. Change the currently selected left side menu item from Overview to Reports.
@@ -54,7 +60,6 @@ table.classList.add('table-hover');
 var rows = document.querySelectorAll('tr');
 for (var i = 7; i <= 10; i++) {
     rows[i].classList.add('success');
-    console.log(rows);
 }
 
 
@@ -62,14 +67,14 @@ for (var i = 7; i <= 10; i++) {
 var heading = document.querySelector('thead');
 var headingRow = heading.children[0];
 
-rowIDs(0).innerText = 'ID';
-rowIDs(1).innerText = 'First Name';
-rowIDs(2).innerText = 'Last Name';
-rowIDs(3).innerText = 'Department';
-rowIDs(4).innerText = 'Client';
+rowIDs(0, 'ID');
+rowIDs(1, 'First Name');
+rowIDs(2, 'Last Name');
+rowIDs(3, 'Department');
+rowIDs(4, 'Client');
 
-function rowIDs(position) {
-    return headingRow.children[position];
+function rowIDs(position, headerName) {
+    return headingRow.children[position].innerText = headerName;
 }
 
 
@@ -82,32 +87,32 @@ for (var i = 1; i < rowArray.length; i++) {
     var noComma = Number(firstItems.innerText.replace(/,/g , ''))
     firstItems.innerText = noComma;
 
-var object = {};
-object.id = rowArray[i].children[0].innerHTML;
-object.firstName = rowArray[i].children[1].innerHTML;
-object.lastName = rowArray[i].children[2].innerHTML;
-object.department = rowArray[i].children[3].innerHTML;
-object.client = rowArray[i].children[4].innerHTML;
-objectArray.push(object);
+    var object = {};
+    object.id = Number(rowArray[i].children[0].innerText.replace(/,/g, ''));
+    object.firstName = rowArray[i].children[1].innerHTML;
+    object.lastName = rowArray[i].children[2].innerHTML;
+    object.department = rowArray[i].children[3].innerHTML;
+    object.client = rowArray[i].children[4].innerHTML;
+
+    objectArray.push(object);
 }
 
-console.log(objectArray);
+    console.log(objectArray);
+
 
 // 10. Make each word in the table capitalized.
-for(var i = 1; i < rowArray.length; i++) {
-
+for (var i = 1; i < rowArray.length; i++) {
     allItems(1);
     allItems(2);
     allItems(3);
     allItems(4)
-
 }
 
 function allItems(position) {
     var items = rowArray[i].children[position];
-
-    var item = items.innerText;
-    var itemEnding = item.slice(1);
-    var capitalFirstLetter = item.charAt(0).toUpperCase();
-    items.innerText  = capitalFirstLetter + itemEnding;
+    // var item = items.innerText;
+    // var itemEnding = item.slice(1);
+    // var capitalFirstLetter = item.charAt(0).toUpperCase();
+    // items.innerText = capitalFirstLetter + itemEnding;
+    items.classList = "text-capitalize";
 }
